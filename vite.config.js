f499a4255321e863f6ffe25d40ee5a47f4cfed29
Mokamatala Sri Naga Manikanta <manikanta52848@gmail.com>
2025-07-28 10:26:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/S222-master/' : '/',
+  base: '/S222-master/'
 });
